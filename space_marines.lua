1408b6ad779019937b8180748c4782af469e741a
Lokathor <zefria@gmail.com>
2025-07-24 14:42:51 -0600

    Update space_marines.lua

--- space_marines.lua
+++ space_marines.lua
@@ -1,9 +1,11 @@
 
 require "utils"
 
-SPACE_MARINE_WEAPONS = {}
+space_marines.weapons = {}
+space_marines.models = {}
+space_marines.datasheets = {}
 
-SPACE_MARINE_WEAPONS["Bolt Rifle (focused)"] = Weapon{
+space_marines.weapons.bolt_rifle_focused = Weapon{
   name = "Bolt Rifle (focused)",
   range = 24,
   num_attacks = 4,
@@ -14,18 +16,7 @@ SPACE_MARINE_WEAPONS["Bolt Rifle (focused)"] = Weapon{
   attrs = { assault=true, heavy=true },
 }
 
-SPACE_MARINE_WEAPONS["Grenade Launcher (frag)"] = Weapon{
-  name = "Grenade Launcher (frag)",
-  range = 24,
-  num_attacks = "d3",
-  to_hit = 3,
-  strength = 4,
-  ap = 0,
-  damage = 1,
-  attrs = { blast = true },
-}
-
-SPACE_MARINE_WEAPONS["Grenade Launcher (krak)"] = Weapon{
+space_marines.weapons.krak_grenade_launcher = Weapon{
   name = "Grenade Launcher (krak)",
   range = 24,
   num_attacks = 1,
@@ -35,7 +26,18 @@ SPACE_MARINE_WEAPONS["Grenade Launcher (krak)"] = Weapon{
   damage = "d3",
 }
 
-SPACE_MARINE_WEAPONS["Heavy Bolt Pistol"] = Weapon{
+space_marines.weapons.bolt_pistol_default = Weapon{
+  name = "Bolt Pistol",
+  range = 12,
+  num_attacks = 1,
+  to_hit = 3,
+  strength = 4,
+  ap = 0,
+  damage = 1,
+  attrs = { pistol = true },
+}
+
+space_marines.weapons.heavy_bolt_pistol_default =  Weapon{
   name = "Heavy Bolt Pistol",
   range = 18,
   num_attacks = 1,
@@ -46,18 +48,38 @@ SPACE_MARINE_WEAPONS["Heavy Bolt Pistol"] = Weapon{
   attrs = { pistol = true },
 }
 
-SPACE_MARINE_WEAPONS["Plasma Pistol (normal)"] = Weapon{
+space_marines.weapons.plasma_pistol_normal_default = Weapon{
   name = "Plasma Pistol (normal)",
   range = 12,
   num_attacks = 1,
   to_hit = 3,
   strength = 7,
-  ap = 1,
+  ap = 2,
   damage = 1,
   attrs = { pistol = true },
 }
 
-SPACE_MARINE_WEAPONS["Master-crafted Power Weapon"] = Weapon{
+space_marines.weapons.close_combat_default = Weapon{
+  name = "Close combat weapon",
+  range= 1,
+  num_attacks=3,
+  to_hit = 3,
+  strength=4,
+  ap= 0,
+  damage = 1,
+}
+
+space_marines.weapons.power_fist_3_3 = Weapon{
+  name = "Power Fist",
+  range= 1,
+  num_attacks=3,
+  to_hit = 3,
+  strength=8,
+  ap= 2,
+  damage = 2,
+}
+
+space_marines.weapons.bladeguard_sword = Weapon{
   name = "Master-crafted Power Weapon",
   range = 1,
   num_attacks = 4,
@@ -67,7 +89,7 @@ SPACE_MARINE_WEAPONS["Master-crafted Power Weapon"] = Weapon{
   damage = 2,
 }
 
-BASIC_INTERCESSOR = Model{
+space_marines.models.basic_intercessor = Model{
   name = "Intercessor",
   movement = 6,
   toughness = 4,
@@ -75,18 +97,51 @@ BASIC_INTERCESSOR = Model{
   wounds = 2,
   leadership = 6,
   oc = 2,
-  weapons = {INTERCESSOR_FOCUSED_BOLT_RIFLE},
+  weapons = {
+    space_marines.weapons.bolt_rifle_focused,
+    space_marines.weapons.bolt_pistol_default,
+    space_marines.weapons.close_combat_default,
+  },
 }
 
-BASIC_INTERCESSOR_SQUAD = Unit{
+space_marines.models.intercessor_krak = Model{
+  name = "Intercessor",
+  movement = 6,
+  toughness = 4,
+  armor_save = 3,
+  wounds = 2,
+  leadership = 6,
+  oc = 2,
+  weapons = {
+    space_marines.weapons.bolt_rifle_focused,
+    space_marines.weapons.krak_grenade_launcher,
+    space_marines.weapons.bolt_pistol_default,
+    space_marines.weapons.close_combat_default,
+  }
+},
+
+space_marines.models.intercessor_fist_rifle = Model{
+  name = "Intercessor Sergeant",
+  movement = 6,
+  toughness = 4,
+  armor_save = 3,
+  wounds = 2,
+  leadership = 6,
+  oc = 2,
+  weapons = {
+    space_marines.weapons.bolt_rifle_focused,
+    space_marines.weapons.bolt_pistol_default,
+    space_marines.weapons.power_fist_3_3,
+  }
+},
+
+space_marines.datasheets.intercessor_squad = Unit{
   name = "Intercessors",
   models = {
-    recursive_clone(BASIC_INTERCESSOR),
-    recursive_clone(BASIC_INTERCESSOR),
-    recursive_clone(BASIC_INTERCESSOR),
-    recursive_clone(BASIC_INTERCESSOR),
-    recursive_clone(BASIC_INTERCESSOR),
+    space_marines.models.basic_intercessor,
+    space_marines.models.basic_intercessor,
+    space_marines.models.basic_intercessor,
+    space_marines.models.intercessor_krak,
+    space_marines.models.intercessor_fist_rifle,
   }
 }
-BASIC_INTERCESSOR_SQUAD.models[4].weapons[2] = recursive_clone(INTERCESSOR_KRAK_GRENADE_LAUNCHER)
-BASIC_INTERCESSOR_SQUAD.models[5].weapons[1] = recursive_clone(PLASMA_PISTOL_NORMAL)
