87adec49bdb60b459b586cfea76f172b274b9ed2
lokathor <zefria@gmail.com>
2025-12-28 18:37:12 -0700

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use randomize::Gen32;
 use randomize::PCG32;
 
+pub mod units;
+pub use units::*;
+
 #[allow(unused)]
 pub fn do_shooting(
   attacker: &mut Unit, defender: &mut Unit, distance: u8, cover: f32,
@@ -80,156 +83,6 @@ pub fn do_shooting(
   }
 }
 
-pub fn gladiator_lancer_w_grenades() -> Unit {
-  Unit {
-    name: "Galdiator Lancer".into(),
-    models: vec![Model {
-      name: "Gladiator Lancer".into(),
-      speed: 10,
-      toughness: 10,
-      armor: 3,
-      health: 12,
-      guns: vec![
-        Weapon {
-          name: "Lancer Laser Destroyer".into(),
-          range: 72,
-          attacks: Expr::_2,
-          skill: 3,
-          strength: 14,
-          ap: 4,
-          damage: Expr::D6(1, 3),
-          rules: vec![WeaponRule::Heavy],
-          ..Default::default()
-        },
-        Weapon {
-          name: "Icarus Rocket Pod".into(),
-          range: 24,
-          attacks: Expr::D3(1, 0),
-          skill: 3,
-          strength: 8,
-          ap: 1,
-          damage: Expr::_2,
-          rules: vec![WeaponRule::AntiFly2],
-          ..Default::default()
-        },
-        Weapon {
-          name: "Ironhail Heavy Stubber".into(),
-          range: 36,
-          attacks: Expr::_3,
-          skill: 3,
-          strength: 4,
-          ap: 0,
-          damage: Expr::_1,
-          rules: vec![WeaponRule::RapidFire3],
-          ..Default::default()
-        },
-        Weapon {
-          name: "Fragstorm Grenade Launcher".into(),
-          range: 18,
-          attacks: Expr::D6(1, 0),
-          skill: 3,
-          strength: 4,
-          ap: 0,
-          damage: Expr::_1,
-          rules: vec![WeaponRule::Blast],
-          ..Default::default()
-        },
-        Weapon {
-          name: "Fragstorm Grenade Launcher".into(),
-          range: 18,
-          attacks: Expr::D6(1, 0),
-          skill: 3,
-          strength: 4,
-          ap: 0,
-          damage: Expr::_1,
-          rules: vec![WeaponRule::Blast],
-          ..Default::default()
-        },
-      ],
-      sticks: vec![Weapon {
-        name: "Armored_Hull".into(),
-        range: 1,
-        attacks: Expr::_3,
-        skill: 4,
-        strength: 6,
-        ap: 0,
-        damage: Expr::_1,
-        ..Default::default()
-      }],
-      rules: vec![
-        ModelRule::Vehicle,
-        ModelRule::Smoke,
-        ModelRule::Imperium,
-        ModelRule::EagleOptics,
-      ],
-      ..Default::default()
-    }],
-  }
-}
-
-pub fn ballistus_dreadnought_krak() -> Unit {
-  Unit {
-    name: "Ballistus Dreadnought".into(),
-    models: vec![Model {
-      name: "Ballistus Dreadnought".into(),
-      speed: 8,
-      toughness: 10,
-      armor: 2,
-      health: 12,
-      guns: vec![
-        Weapon {
-          name: "Ballistus Lascannon".into(),
-          range: 48,
-          attacks: Expr::_2,
-          skill: 3,
-          strength: 12,
-          ap: 3,
-          damage: Expr::D6(1, 1),
-          rules: vec![],
-        },
-        Weapon {
-          name: "Ballistus Missile Launcher (Krak)".into(),
-          range: 48,
-          attacks: Expr::_2,
-          skill: 3,
-          strength: 10,
-          ap: 2,
-          damage: Expr::D6(1, 0),
-          rules: vec![],
-        },
-        Weapon {
-          name: "Twin-linked Storm Bolter".into(),
-          range: 24,
-          attacks: Expr::_2,
-          skill: 3,
-          strength: 4,
-          ap: 0,
-          damage: Expr::_1,
-          rules: vec![WeaponRule::RapidFire2, WeaponRule::TwinLinked],
-        },
-      ],
-      sticks: vec![Weapon {
-        name: "Armored Feet".into(),
-        range: 1,
-        attacks: Expr::_5,
-        skill: 3,
-        strength: 7,
-        ap: 0,
-        damage: Expr::_1,
-        rules: vec![],
-      }],
-      rules: vec![
-        ModelRule::Vehicle,
-        ModelRule::Walker,
-        ModelRule::Imperium,
-        ModelRule::Dreadnought,
-        ModelRule::BallistusStrike,
-      ],
-      ..Default::default()
-    }],
-  }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Unit {
   pub name: String,
@@ -300,6 +153,14 @@ impl Expr {
       }
     }
   }
+  
+  pub fn max_roll(&self) -> u8 {
+    match self {
+      Self::F(x) => x,
+      Self::D3(x,y) => x*3+y,
+      Self::D6(x,y) => x*6+y,
+    }
+  }
 }
 impl Default for Expr {
   #[inline]
@@ -317,14 +178,17 @@ pub enum ModelRule {
   Walker,
   Dreadnought,
   BallistusStrike,
+  DarkPacts,
+  Infantry,
+  Chaos,
+  DeepStrike,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum WeaponRule {
   Heavy,
-  AntiFly2,
-  RapidFire2,
-  RapidFire3,
+  Anti(ModelRule,u8),
+  RapidFire(Expr),
   Blast,
   TwinLinked,
 }
---
+++ src/units.rs
@@ -0,0 +1,151 @@
+use super::*;
+
+pub fn gladiator_lancer_w_grenades() -> Unit {
+  Unit {
+    name: "Galdiator Lancer".into(),
+    models: vec![Model {
+      name: "Gladiator Lancer".into(),
+      speed: 10,
+      toughness: 10,
+      armor: 3,
+      health: 12,
+      guns: vec![
+        Weapon {
+          name: "Lancer Laser Destroyer".into(),
+          range: 72,
+          attacks: Expr::_2,
+          skill: 3,
+          strength: 14,
+          ap: 4,
+          damage: Expr::D6(1, 3),
+          rules: vec![WeaponRule::Heavy],
+          ..Default::default()
+        },
+        Weapon {
+          name: "Icarus Rocket Pod".into(),
+          range: 24,
+          attacks: Expr::D3(1, 0),
+          skill: 3,
+          strength: 8,
+          ap: 1,
+          damage: Expr::_2,
+          rules: vec![WeaponRule::Anti(ModelRule::Fly,2)],
+          ..Default::default()
+        },
+        Weapon {
+          name: "Ironhail Heavy Stubber".into(),
+          range: 36,
+          attacks: Expr::_3,
+          skill: 3,
+          strength: 4,
+          ap: 0,
+          damage: Expr::_1,
+          rules: vec![WeaponRule::RapidFire3],
+          ..Default::default()
+        },
+        Weapon {
+          name: "Fragstorm Grenade Launcher".into(),
+          range: 18,
+          attacks: Expr::D6(1, 0),
+          skill: 3,
+          strength: 4,
+          ap: 0,
+          damage: Expr::_1,
+          rules: vec![WeaponRule::Blast],
+          ..Default::default()
+        },
+        Weapon {
+          name: "Fragstorm Grenade Launcher".into(),
+          range: 18,
+          attacks: Expr::D6(1, 0),
+          skill: 3,
+          strength: 4,
+          ap: 0,
+          damage: Expr::_1,
+          rules: vec![WeaponRule::Blast],
+          ..Default::default()
+        },
+      ],
+      sticks: vec![Weapon {
+        name: "Armored_Hull".into(),
+        range: 1,
+        attacks: Expr::_3,
+        skill: 4,
+        strength: 6,
+        ap: 0,
+        damage: Expr::_1,
+        ..Default::default()
+      }],
+      rules: vec![
+        ModelRule::Vehicle,
+        ModelRule::Smoke,
+        ModelRule::Imperium,
+        ModelRule::EagleOptics,
+      ],
+      ..Default::default()
+    }],
+  }
+}
+
+pub fn ballistus_dreadnought_krak() -> Unit {
+  Unit {
+    name: "Ballistus Dreadnought".into(),
+    models: vec![Model {
+      name: "Ballistus Dreadnought".into(),
+      speed: 8,
+      toughness: 10,
+      armor: 2,
+      health: 12,
+      guns: vec![
+        Weapon {
+          name: "Ballistus Lascannon".into(),
+          range: 48,
+          attacks: Expr::_2,
+          skill: 3,
+          strength: 12,
+          ap: 3,
+          damage: Expr::D6(1, 1),
+          rules: vec![],
+        },
+        Weapon {
+          name: "Ballistus Missile Launcher (Krak)".into(),
+          range: 48,
+          attacks: Expr::_2,
+          skill: 3,
+          strength: 10,
+          ap: 2,
+          damage: Expr::D6(1, 0),
+          rules: vec![],
+        },
+        Weapon {
+          name: "Twin-linked Storm Bolter".into(),
+          range: 24,
+          attacks: Expr::_2,
+          skill: 3,
+          strength: 4,
+          ap: 0,
+          damage: Expr::_1,
+          rules: vec![WeaponRule::RapidFire(2), WeaponRule::TwinLinked],
+        },
+      ],
+      sticks: vec![Weapon {
+        name: "Armored Feet".into(),
+        range: 1,
+        attacks: Expr::_5,
+        skill: 3,
+        strength: 7,
+        ap: 0,
+        damage: Expr::_1,
+        rules: vec![],
+      }],
+      rules: vec![
+        ModelRule::Vehicle,
+        ModelRule::Walker,
+        ModelRule::Imperium,
+        ModelRule::Dreadnought,
+        ModelRule::BallistusStrike,
+      ],
+      ..Default::default()
+    }],
+  }
+}
